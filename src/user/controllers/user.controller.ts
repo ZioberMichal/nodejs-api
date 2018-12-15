3b3678f4df3e6f7c09434f4b24749da80c06af77
Michał Ziober <ziobermichal@gmail.com>
2018-12-15 13:08:40 +0100

    Created async function for regeister method
    Created async function for regeister method.
    Added form with POST.

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -18,14 +18,33 @@ export class UserController {
         return body;
     }
 
+    @Get('register')
+    getRegister() {
+        return '<form action="/user/register" method="post"><input type="text" name="name" placeholder="Name"><input type="text" name="email" placeholder="Email"><input type="submit"></form>';
+    }
+
     @Post('register')
-    register(@Body() data: UserRegisterRequestDto): UserRegisterResponseDto {
-        return {
+    async register(@Body() data: UserRegisterRequestDto): Promise<UserRegisterResponseDto> {
+        const user = {
             user: {
                 id: 1,
-                name: 'Michal',
-                email: 'email@email.com',
+                name: data.name,
+                email: data.email,
             },
         };
+
+        return delay(2, user);
     }
+}
+
+function delay(time = 2, data: UserRegisterResponseDto): Promise<UserRegisterResponseDto> {
+    return new Promise((resolve, reject) => {
+        if (time > 2) {
+            reject('TOO LONG!');
+        } else {
+            setTimeout(() => {
+                resolve(data);
+            }, time * 1000);
+        }
+    });
 }
